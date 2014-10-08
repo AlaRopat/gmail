eb2eeae5e382bcf980e4f8a906b86f6ad0af0e09
Ala Ropat <ala_ropat@epam.com>
2014-10-08 17:30:26 +0300

    my project

---
+++ src/test/java/com/epam/test/Spam.java
@@ -0,0 +1,49 @@
+package com.epam.test;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.epam.object.User;
+import com.epam.steps.Step;
+
+public class Spam {
+	private Step steps;
+	private User user1 = User.USER1;
+	private User user2 = User.USER2;
+	private User user3 = User.USER3;
+
+	@BeforeMethod(description = "Init browser")
+	public void setUp() {
+		steps = new Step();
+		steps.initBrowser();
+
+	}
+
+	@Test
+	public void markLetterFromUser1AsSpamGoToFolderSpam() {
+		steps.loginGmail(user1);
+		steps.sendLetter(user2, 25, "one");
+		steps.closeDriver();
+		steps.initBrowser();
+		steps.loginGmail(user2);
+		steps.markLetterFromUser1AsSpam();
+		steps.closeDriver();
+		steps.initBrowser();
+		steps.loginGmail(user1);
+		steps.sendLetter(user2, 25, "two");
+		steps.closeDriver();
+		steps.initBrowser();
+		steps.loginGmail(user2);
+		steps.goToFolderSpam();
+		Assert.assertTrue(steps.letterInSpam());
+	}
+
+	@AfterMethod(description = "Stop Browser")
+	public void stopBrowser() {
+		steps.closeDriver();
+	}
+
+}
